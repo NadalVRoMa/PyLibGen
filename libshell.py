4d4d6433da64ef2fac9cc65b00752132bfec398c
Nadal Rodrigo <nadalvroma@gmail.com>
2017-10-20 01:20:39 +0200

    Add files via upload

--- libshell.py
+++ libshell.py
@@ -5,8 +5,7 @@ import os
 from tabulate import tabulate
 
 
-def getSearchResults(term, column='def'):
-    page = 1
+def getSearchResults(term, page, column='def'):
     url = 'http://libgen.io/search.php?&req={}&column={}&page={}'.format(
         term, column, str(page))
     # URGENT : ESCAPE SYMBOLS!!
@@ -16,76 +15,70 @@ def getSearchResults(term, column='def'):
     page_books = soup.find_all('tr')
     page_books = page_books[3:-1]  # Ignore 3 first and the last <tr> label.
     books = page_books
-
-    # TODO: Add functionality to get first results if there are too many.
-
-    while len(page_books) == 25:  # The max number of books in a page is 25.
-        page += 1
-        url = 'http://libgen.io/search.php?&req={}&column={}&page={}'.format(
-            term, column, str(page))
-
-        source = req.urlopen(url)
-        soup = bs4.BeautifulSoup(source, 'lxml')
-        page_books = soup.find_all('tr')
-        page_books = page_books[3:-1]
-        books += page_books
-
     return(books)
 
 
-def displayBooks(books):
+def formatBooks(books, page):
     # TODO: Add support for multiple choices
     fmt_books = []
-    titles = []
+    books_mirrors = [] # List of dics with complete titles and mirrors
+
     for i, rawbook in enumerate(books):
+        i += (page - 1) * 25
+
         book_attrs = rawbook.find_all('td')
-        author = book_attrs[1].a.text  # only first author
+        author = book_attrs[1].a.text[:25]  # only first author
         title = book_attrs[2].find(title=True).text
-        tinytitle = title[:100]
-        publisher = book_attrs[3].text
+        tinytitle = title[:80]
+        publisher = book_attrs[3].text[:25]
         year = book_attrs[4].text
         lang = book_attrs[6].text[:2]  # Show only 2 first characters
         size = book_attrs[7].text
         ext = book_attrs[8].text
+        io_mirror = book_attrs[9].a.attrs['href']
+
         book = [str(i + 1), author, tinytitle, publisher,
                 year, lang, ext, size]  # Start at 1
-        titles.append(title)
+
+        book_mirrors = {'title': title, 'io': io_mirror}
+        books_mirrors.append(book_mirrors)
+
         fmt_books.append(book)
 
+    return(fmt_books, books_mirrors)
+
+
+def selectBook(books, mirrors, page, end=False):
     headers = ['#', 'Author', 'Title', 'Publisher',
                'Year', 'Lang', 'Ext', 'Size']
 
-    cont = True
-    i = 0
-    while cont:
-        print(tabulate(fmt_books[i * 25:(i + 1) * 25], headers))
-        while True:
-            print('\n Type the number of your choice to start the download.')
-            elec = input('Press Enter to see more matches or type q to quit: ')
+    if not end:
+        print(tabulate(books[(page - 1) * 25:page * 25], headers))
+    else:
+        print('Sorry, no more matches.')
 
-            if elec.isnumeric():
-                choice = int(elec)
-                if choice <= (i + 1) * 25:  # Selection
-                    return([choice, titles[choice], fmt_books[choice][6]])
-                else:
-                    print("Too big of a number.")
-                    continue
+    while True:
+        elec = input(
+            '\n Type # of book to download, q to quit or just press Enter to see more matches: ')
 
-            elif elec == 'q' or elec == 'Q':  # Quit
+        if elec.isnumeric():
+            choice = int(elec) - 1
+            if choice < len(books):  # Selection
+                title = '{}.{}'.format(mirrors[choice]['title'], books[choice][-2])
+                downloadBook(mirrors[choice]['io'], title)
                 return(False)
+            else:
+                print("Too big of a number.")
+                continue
 
-            elif not elec:  # See more matches
-                if (i + 1) * 25 >= len(books):
-                    print("There aren't more matches")
-                    continue
-                else:
-                    i += 1
-                    break
+        elif elec == 'q' or elec == 'Q':  # Quit
+            return(False)
+
+        elif not elec:  # See more matches
+            return(True)
 
 
-def downloadBook(book, filename):
-    io_mirror = book.find_all('td')[-5]
-    link = io_mirror.a.attrs['href']
+def downloadBook(link, filename):
     source = req.urlopen(link)
     soup = bs4.BeautifulSoup(source, 'lxml')
 
@@ -105,12 +98,18 @@ if __name__ == '__main__':
     parser.add_argument('-s', '--search', required=True, dest='search_term')
     args = parser.parse_args()
 
-    books = getSearchResults(args.search_term)
-    if books:
-        ch_book = displayBooks(books)
-        if ch_book:
-            book = books[ch_book[0]]
-            title = '{}.{}'.format(ch_book[1], ch_book[2])
-            downloadBook(book, title)
-    else:
-        print('Sorry, no matches found.')
+    books = []
+    mirrors = []
+    page = 1
+    get_next_page = True
+
+    while get_next_page:
+        raw_books = getSearchResults(args.search_term, page)
+        if raw_books:
+            new_books, new_mirrors = formatBooks(raw_books, page)
+            books += new_books
+            mirrors += new_mirrors
+            get_next_page = selectBook(books, mirrors, page)
+            page += 1
+        else:
+            get_next_page = selectBook(books, mirrors, page - 1, end=True)
