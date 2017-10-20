99c376462107664f322bcb8099a6df383fd4f2bf
nadal <nadalvroma@gmail.com>
2017-10-20 17:09:52 +0200

    typo

--- settings.py
+++ settings.py
@@ -1,5 +1,5 @@
 DOWNLOAD_PATH = "." # Where do you want the books to be downloaded. Default is the script directory.
-N_AUTHORS = 1 # Maximum of authours displayed.
+N_AUTHORS = 1 # Maximum of authors displayed.
 MAX_CHARS_AUTHORS = 25 # Maximum characters displayed for the author. Change according to N_AUTHORS.
 MAX_CHARS_TITLE = 50 # Maximum characters displayed for the book title
 MAX_CHARS_PUBLISHER = 25 # Maximum characters displayed for the publisher.
