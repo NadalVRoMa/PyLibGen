10e06122268aaf330931e32e18f0b3ca5fa843d2
Nadal Rodrigo <nadalvroma@gmail.com>
2017-10-20 18:22:54 +0200

    Update settings.py
    set 20 for MAX_CHARS_PUBLISHER

--- settings.py
+++ settings.py
@@ -2,4 +2,4 @@ DOWNLOAD_PATH = "." # Where do you want the books to be downloaded. Default is t
 N_AUTHORS = 1 # Maximum of authors displayed.
 MAX_CHARS_AUTHORS = 25 # Maximum characters displayed for the author. Change according to N_AUTHORS.
 MAX_CHARS_TITLE = 50 # Maximum characters displayed for the book title
-MAX_CHARS_PUBLISHER = 25 # Maximum characters displayed for the publisher.
+MAX_CHARS_PUBLISHER = 20 # Maximum characters displayed for the publisher.
