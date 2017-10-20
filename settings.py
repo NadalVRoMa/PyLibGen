1e3fa4b784dbb9152a75de165e01af1f2ab0dc6d
nadal <nadalvroma@gmail.com>
2017-10-20 09:29:57 +0200

    add settings.py

---
+++ settings.py
@@ -0,0 +1,7 @@
+DOWNLOAD_PATH = "." # Where do you want the books to be downloaded. Default is the script directory.
+RESULTS_PER_PAGE = 25 # Results displayed per page.
+MAX_CHARS_TITLE = 80 # Maximum characters displayed for the book title
+N_AUTHORS = 1 # Maximum of authours displayed.
+MAX_CHARS_AUTHOR = 25 # Maximum characters displayed for the author. Change according to N_AUTHORS.
+MAX_CHARS_PUBLISHER = 25 # Maximum characters displayed for the publisher.
+FIRST_MIRROR = 'io' # First mirror to check.
