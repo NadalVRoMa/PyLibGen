ecb7858f38ef3295b765416275f7ebdd46ed5137
nadal <nadalvroma@gmail.com>
2017-10-20 11:13:39 +0200

    set defaults for settings.py

--- settings.py
+++ settings.py
@@ -1,5 +1,5 @@
-DOWNLOAD_PATH = "/home/nadal/Downloads" # Where do you want the books to be downloaded. Default is the script directory.
-MAX_CHARS_TITLE = 50 # Maximum characters displayed for the book title
+DOWNLOAD_PATH = "." # Where do you want the books to be downloaded. Default is the script directory.
 N_AUTHORS = 1 # Maximum of authours displayed.
 MAX_CHARS_AUTHORS = 25 # Maximum characters displayed for the author. Change according to N_AUTHORS.
+MAX_CHARS_TITLE = 50 # Maximum characters displayed for the book title
 MAX_CHARS_PUBLISHER = 25 # Maximum characters displayed for the publisher.
