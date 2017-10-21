700abeae5ea0f843f66b24bf7c614414f86c55b4
nadal <nadalvroma@gmail.com>
2017-10-21 13:25:42 +0200

    removed MULTIPLE_MIRRORS

--- settings.py
+++ settings.py
@@ -3,4 +3,3 @@ N_AUTHORS = 1 # Maximum of authors displayed.
 MAX_CHARS_AUTHORS = 25 # Maximum characters displayed for the author. Change according to N_AUTHORS.
 MAX_CHARS_TITLE = 50 # Maximum characters displayed for the book title
 MAX_CHARS_PUBLISHER = 20 # Maximum characters displayed for the publisher.
-MULTIPLE_MIRRORS = False # Multiple Mirrors to download
